584a793054536ae50db14cdb76774dd9bff425d4
sayanidey29 <sayanidey199829@gmail.com>
2024-09-11 11:43:52 +0530

    chatbot backend

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.get("/", (req, res) => {
   res.send(200, "Hi Sayani");
 });
 
+app.post("/getanswer", (req, res) => {
+  setTimeout(() => {
+    res.send(200, "I am BOT");
+  }, 500);
+});
+
 app.listen(3030, async () => {
   console.log("Working on http://localhost:3030");
 });
